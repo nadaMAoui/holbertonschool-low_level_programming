d8c5c60e4e4af6d19b2e220a0c2a874ec02010eb
nadaMAoui <maaouinada747@gmail.com>
2022-09-29 15:55:11 -0700

    mmm

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(void)
+/*
+ *  C program that prits a string
+ */
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
 }
