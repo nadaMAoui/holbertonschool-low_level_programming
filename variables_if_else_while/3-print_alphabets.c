d20aa1e7ce0d2f60f9f5a75783a68df7ce81d175
nadaMAoui <maaouinada747@gmail.com>
2022-10-03 11:41:57 -0700

    print the alphabet in lowercase, and then in uppercase,followed by a new line..

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,22 +6,19 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
-	
+int main(void)
+{
 	int i;
 
-	
-	for (i= 97, <= 122, i++)
+	for (i = 97; i <= 122; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
-	for (i= 65, <= 90, i++)
+	for (i = 65; i <= 90; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
-	putchar("\n");
-
+	putchar('\n');
 
 	return (0);
 }
