652043ab7d53adbd0fb8a6dd581fb7d998d47b53
nadaMAoui <maaouinada747@gmail.com>
2022-10-03 16:16:49 -0700

    print all possible combinations of single-digit numbers x

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,14 +11,15 @@ int main(void)
 {
 int i;
 
-for (i = 48; i <= 56; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
+if (i != 57)
+{
 putchar(',');
 putchar(' ');
 }
-putchar(57);
+}
 putchar('\n');
 return (0);
 }
-
