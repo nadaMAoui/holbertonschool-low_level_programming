baeb80f88a339576470d0644652c51be857f32fe
nadaMAoui <maaouinada747@gmail.com>
2022-10-03 16:22:43 -0700

    print whether the number stored in the variable n is positive or negative x2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,29 +5,29 @@
 /**
  * main - entry to the main
  *
- * description : 'priintf a sorted var is positive or negative'
+ * description : 'print if a sorted var is positive or negative'
  *
  * return: return 0 in sucess
  */
 int main(void)
 
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
-		int n;
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-
-		if (n > 0)
-		{
-			printf("%d is positive\n", n);
-		}
-		else if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
-		else
-		{	
-			printf("%d is negative\n", n);
-		}
-		return (0);
+		printf("%d is positive\n", n);
 	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
