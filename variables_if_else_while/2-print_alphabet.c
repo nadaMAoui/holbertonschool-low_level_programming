ca24325cf91936d6ee574f8b238efd095ab256f9
nadaMAoui <maaouinada747@gmail.com>
2022-10-03 07:42:53 -0700

    prints the alphabet in lowercase \n

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 97; i<=122; i++)
+	{
+	    putchar(i);
+	}
+	putchar("\n");
+
+	return(0)
+}    
+
