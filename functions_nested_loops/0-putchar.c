e05a5adaf49c618e5d10f7a64bb39edc3a79d7aa
nadaMAoui <maaouinada747@gmail.com>
2022-10-04 02:25:15 -0700

    prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Enter main
+ *
+ * Description: 'prints _putchar'
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{	
+	int string; 
+
+	{ 
+		printf("_putchar");
+	}
+
+
+	printf("\n");
+	return (0);
+}
