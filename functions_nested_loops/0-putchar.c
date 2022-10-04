733379f178d4b64c0c9a5a5db4c3757caaac94b7
nadaMAoui <maaouinada747@gmail.com>
2022-10-04 03:50:56 -0700

    print _putchar x1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,13 +8,19 @@
  * Return: return 0 (success)
  */
 int main(void)
-{ 
+{
 
-	{ 
-		printf("_putchar");
-	}
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
 
 
-	printf("\n");
 	return (0);
 }
