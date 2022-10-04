de791699be1eb47cd452a324fcf2467951ffca0e
nadaMAoui <maaouinada747@gmail.com>
2022-10-04 16:13:33 -0700

    check for lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * _islower - Entry point
+ *
+ * @c: integer
+ *
+ * Description: 'checks for lowercase character'
+ *
+ * Return: Return 1 (fail) or 0 (sccess)
+*/
+int _islower(int c)
+{
+
+int i;
+int c;
+
+for (i = 97; i <= 122; i++)
+{
+	if ((i < 97 )) or (( i > 122 ));
+}
+{
+	putchar('0');
+}
+else if
+{
+	putchar( (c= char (i);) + '0');
+}
+
+putchar('\n');
+return (0);
+}
+}
