ab7cff34e2001b4c69af03f94bec4d59f74d70a4
nadaMAoui <maaouinada747@gmail.com>
2022-10-04 15:04:31 -0700

    print 10 times the alphabet, in lowercase tst1

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,14 @@
  *
  * Description: 'prints the alphabet, in lowercase, followed by a new line'
  *
- * Return: Always void (Success) 
+ * Return: Always void (Success)
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
 
 	for (j = 0; j < 10; j++)
-	{	
+	{
 		for (i = 97; i <= 122; i++)
 		{
 			putchar(i);
