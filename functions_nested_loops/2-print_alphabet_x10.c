31d2b60970ef19cea320480513df9e223a0106cc
nadaMAoui <maaouinada747@gmail.com>
2022-10-04 14:13:36 -0700

    print 10 times the alphabet, in lowercase x6

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,13 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	{
+	
 		for (j = 0; i <= 9; j++)
 		{	
 			for (i = 97; i <= 122; i++)
 			{
 				putchar(i);
 			}
-		}
-	}
+		
 	putchar('\n');
 }
