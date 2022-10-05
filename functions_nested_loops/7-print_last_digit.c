f287c3e089eaf25e28cf2f94a04399a25db01fcd
nadaMAoui <maaouinada747@gmail.com>
2022-10-05 08:44:27 -0700

    prints the last digit of a number mm

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,15 @@
  *
  *@n: integer to use
  *
- *@digit: integer to use 
- *
  * Return: retun digit  (success)
 */
 int print_last_digit(int n)
 	
 {
+        if (n < 0)
+	{
+		_putchar(-n)
+	}
+	_putchar((n % 10) + '0');
 	return (n % 10);
 }
