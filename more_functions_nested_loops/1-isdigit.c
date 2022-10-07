5aeebefbbf9d304c66417c314e0c0da0d6d317dc
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 11:46:19 +0200

    1.isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - 'checks for uppercase character'
+ *
+ * @c: integer to use
+ *
+ * Return: return 1 or 1
+*/
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
