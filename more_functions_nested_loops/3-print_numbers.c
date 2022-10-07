ea9488aa4a29f49ff1dac600119a34795ecbce09
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 12:57:49 +0200

    print number from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - 'prints the numbers, from 0 to 9'
+ *
+ * Return: return 0
+*/
+void print_numbers(void)
+{
+char c = '0';
+ while (c <= '9')
+ {
+    putchar(c);
+    c++
+ }
+}
+ 
