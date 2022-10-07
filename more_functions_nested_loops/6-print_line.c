2eebc0029d5fca1f443e47798471209d32de005b
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 17:53:06 +0200

    print line for nomber

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
- * _putchar - print caracter 
+ * _putchar - print caracter
+ * @n: integer
  */
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= n; i++)
-    {
-    _putchar('_');
-    }
-    if ((n = 0) || (n < 0))
-    {
-        _putchar ('\n');
-    }
-    _putchar ('\n');
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	if ((n = 0) || (n < 0))
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
 }
