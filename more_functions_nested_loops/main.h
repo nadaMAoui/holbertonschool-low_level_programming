6ec65081ed059d6c56708f72694f442cf245eb04
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 17:47:02 +0200

    new

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 int _putchar(char c);
+void more_numbers(void);
+void print_line(int n);
+#endif
