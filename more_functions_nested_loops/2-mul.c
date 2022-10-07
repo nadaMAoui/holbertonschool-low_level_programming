5e7cf4871093cca1e8ea5b7960f84c7a39869621
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 12:13:26 +0200

    multiple  and  b

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,12 +3,12 @@
  * mul - 'multplies two integers'
  *
  * @a: integer to use
- * 
+ *
  * @b: integer to use
- * 
+ *
  * Return: return 1 or 1
 */
 int mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
