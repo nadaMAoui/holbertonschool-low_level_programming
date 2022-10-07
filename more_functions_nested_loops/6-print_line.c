e55a417961ba02091324695c4025d0b86c0b680d
nadaMAoui <maaouinada747@gmail.com>
2022-10-07 17:38:43 +0200

    draws a straight line in the terminal n

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * _putchar - print caracter 
+ */
+void print_line(int n)
+{
+    int i;
+
+    for (i = 1; i <= n; i++)
+    {
+    _putchar('_');
+    }
+    if ((n = 0) || (n < 0))
+    {
+        _putchar ('\n');
+    }
+    _putchar ('\n');
+}
