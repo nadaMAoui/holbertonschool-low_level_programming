ec1959c0abe00699f5c6496bc2e407ffebe8e2a2
nadaMAoui <maaouinada747@gmail.com>
2022-10-10 00:32:34 +0200

    prints the n times table, starting with zero test.

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,24 +8,24 @@
 
 void print_times_table(int n)
 {
-    int row, col;
-    int mult = 0;
+int row, col;
+int mult = 0;
 
-    if (n <= 15 && n >= 0)
+if (n <= 15 && n >= 0)
+{
+    for (row = 0; row <= n; row++)
     {
-        for (row = 0; row <= n; row++)
-        {
-            for (col = 0; col <= n; col++)
-            {
-                mult = (row * col);
-                if (mult < 10)
-                {
-                    if (col != 0)
-                    {
-                        _putchar(' ');
-                        _putchar(' ');
-                        _putchar(' ');
-                    }
+    for (col = 0; col <= n; col++)
+    {
+    mult = (row * col);
+if (mult < 10)
+{
+if (col != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
                     _putchar(mult + '0');
                 }
                 else if (mult < 100)
