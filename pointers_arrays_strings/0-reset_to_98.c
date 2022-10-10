54a09c796999d29055ff2aa2eb2bc33e72bdaeab
nadaMAoui <maaouinada747@gmail.com>
2022-10-10 19:38:59 +0200

    reset input to 98 test1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /*
-*reset_to_98 - change the value of int using pointer to 98
-*@n: pointer
-*return: void
-*/
+ * reset_to_98 - change the value of integer using pointer 
+ * @n: pointer
+ * return: void
+ */
 void reset_to_98(int *n)
 {
     *n = 98;
