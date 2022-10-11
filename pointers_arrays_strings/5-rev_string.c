da84c6ee6ea6a888b7b442ca5884ef314e7831e7
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 10:08:49 +0200

    rev string test1

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * rev_string - reverse a string
  * @s: string to reverse
@@ -8,17 +8,17 @@
 void rev_string(char *s)
 { 
 char temp; 
-int aux = 0; // setting a char[0] use it for swap
+int aux = 0;
 int i = 0;
-while (*(s + i) != '\0') // if s[0] diff de nul, i= i+1/ we got the length of string 
+while (*(s + i) != '\0')
 {
 i = i + 1;
 }
-i = i - 1; // we intialize i at the last char
+i = i - 1;
 
  while (aux < i)
 {
-temp = s[i]; // swap char 
+temp = s[i];
 s[i] = s[aux];
 s[aux] = temp;
 aux ++;
