d55cdd649d141a320a8d7d95df77d0ead6312bcf
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 11:35:48 +0200

    print char with pair ref test

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts2 - prints every other character of a string
- * @str: string  
+ * @str: pointer to char 
  * return: void
 */
 void puts2(char *str)
@@ -10,12 +10,11 @@ void puts2(char *str)
 
 while (str[i] != '\0')
 {
-    
 if (str[i] % 2 == 0)
 {
 putchar(*(str + i));
 }
-i = i+1;
+i = i + 1;
 }
 putchar('\n'); 
 }
