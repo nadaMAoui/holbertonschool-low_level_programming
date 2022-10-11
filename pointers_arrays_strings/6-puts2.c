851c7e366fa3a52557c52ad37e0d4653ef262ee4
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 12:16:58 +0200

    prints every other character of a str

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@
 */
 void puts2(char *str)
 {
-    int i;
-    int len;
+int i;
+int len;
 
 len = strlen(str);
 for (i = 0; i < len; i += 2)
 {
-    putchar(str[i]);
+putchar(str[i]);
 }
 putchar('\n');
 }
