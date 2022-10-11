750d0e11c81d65919dbf3611dab050595954eed1
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 08:17:03 +0200

    returns the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+int _strlen(char *s) 
+{ 
+int i = 0;
+
+while (s[i] != '\0')
+{
+i = i+1;
+}
+return (i);
+}
