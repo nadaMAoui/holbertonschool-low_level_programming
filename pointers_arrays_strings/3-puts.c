4c72f037c7248d6b38caa5c077ed905001329fe6
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 08:35:44 +0200

    print string.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+* _puts - fonction print string
+* @i: inetger 
+* return: void 
+*/
+void _puts(char *str)
+{
+int i = 0; 
+
+while(str[i] != '\0')
+{
+putchar(str[i]);
+i = i + 1;
+}
+putchar('\n');
+}
+
