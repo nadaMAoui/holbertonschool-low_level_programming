02b0f5bfd0848036bae25e572693fda4350530d4
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 08:57:59 +0200

    prints a string, in reverse.

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string reversely 
+ * @s: string to reverse
+ * return: void 
+*/
+void print_rev(char *s)
+{
+int i, len;
+
+len = strlen(s);
+for (i = len - 1; i <= 0; i--)
+{
+putchar (s[i]);
+}
+putchar ('\n');
+}
