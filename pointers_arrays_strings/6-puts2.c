e47c8f496193b2bcbceb60466c3025bb22e4854a
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 11:47:03 +0200

    print char with par ref.

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 - prints every other character of a string
  * @str: pointer to char 
@@ -7,14 +8,15 @@
 void puts2(char *str)
 {
     int i = 0;
+    int len;
 
-while (str[i] != '\0')
+len = strlen(str);
+for (i = 0; i < len; i++)
 {
 if (str[i] % 2 == 0)
 {
-putchar(*(str + i));
+    putchar(*(str + i));
 }
-i = i + 1;
 }
-putchar('\n'); 
+putchar('\n');
 }
