1b36f3c1ac51aa48a9100c10aaa78dcb1b0df517
nadaMAoui <maaouinada747@gmail.com>
2022-10-11 10:11:38 +0200

    rev string test2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,11 +3,11 @@
 /**
  * rev_string - reverse a string
  * @s: string to reverse
- * return: void 
+ * return: void
 */
 void rev_string(char *s)
-{ 
-char temp; 
+{
+char temp;
 int aux = 0;
 int i = 0;
 while (*(s + i) != '\0')
@@ -16,12 +16,12 @@ i = i + 1;
 }
 i = i - 1;
 
- while (aux < i)
+while (aux < i)
 {
 temp = s[i];
 s[i] = s[aux];
 s[aux] = temp;
-aux ++;
-i --;
+aux++;
+i--;
 }
 } 
