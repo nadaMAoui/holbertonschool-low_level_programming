5a9a9c0fafaa212dee629d897ccb77b1f9f672a5
nadaMAoui <maaouinada747@gmail.com>
2022-10-13 12:31:21 +0200

    reverse array of integers test1

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,23 +7,22 @@
  */
 void reverse_array(int *a, int n)
 {
-	int j = 0;
-	int i = 0;
-	int *first, *last;
-    int tmp = 0;
-    n -= 1;
-	first = a;
-	last = a;
-    
-	while (i < n)
-	i = i + 1;
+int j = 0;
+int i = 0;
+int *first, *last;
+int tmp = 0;
+n -= 1;
+first = a;
+last = a;
 
-	while (j <= i)
-	{
-	tmp = *(last + i);
-	*(first + i) = *(last + j);
-	*(last + j) = tmp;
-	j++;
-	i--;
-	}
+while (i < n)
+i = i + 1;
+while (j <= i)
+{
+tmp = *(last + i);
+*(first + i) = *(last + j);
+*(last + j) = tmp;
+j++;
+i--;
+}
 }
