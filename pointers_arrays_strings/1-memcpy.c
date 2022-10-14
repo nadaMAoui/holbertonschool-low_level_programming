dd9736753e139c4494411bbcecaa4c49dc2cc2c8
nadaMAoui <maaouinada747@gmail.com>
2022-10-14 11:32:14 +0200

    copies memory area test

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,18 +4,19 @@
  * _memcpy - function that copies n bytes from src to dest
  * @dest: pointer to dest char
  * @src: pointer to source char
+ * @n: size of byte
  * Return: char
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i = 0;
-    unsigned int j = 0;
+unsigned int i = 0;
+unsigned int j = 0;
 
-    while (i < n)
-    {
-        *(dest + i) = * (src + j);
-        i += 1;
-        j += 1;
-    }
-    return (dest);
+while (i < n)
+{
+*(dest + i) = * (src + j);
+i += 1;
+j += 1;
+}
+return (dest);
 }
