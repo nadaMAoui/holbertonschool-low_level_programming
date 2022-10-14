a765705c770aaaa689f3834126da6bc6cc1300c8
nadaMAoui <maaouinada747@gmail.com>
2022-10-14 11:29:26 +0200

    copies memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies n bytes from src to dest
+ * @dest: pointer to dest char
+ * @src: pointer to source char
+ * Return: char
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    while (i < n)
+    {
+        *(dest + i) = * (src + j);
+        i += 1;
+        j += 1;
+    }
+    return (dest);
+}
