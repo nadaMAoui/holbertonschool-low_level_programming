5206052026ad104bbd65769e14980ae756d6774d
nadaMAoui <maaouinada747@gmail.com>
2022-10-17 22:23:56 +0200

    print strings reverly test 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*_print_rev_recursion - function that prints a string reversly 
-* @s: string to recieve 
-*return: void 
+*_print_rev_recursion - function that prints a string reversly
+* @s: string to recieve
+*return: void
 */
 void _print_rev_recursion(char *s)
 {
 if (*s != '\0')
 {
-_print_rev_recursion (s+1);
+_print_rev_recursion (s + 1);
 _putchar (*s);
 }
 else
