6f3e835ec77b69753bef605c74527a47cddfbff4
nadaMAoui <maaouinada747@gmail.com>
2022-10-18 12:40:01 +0200

    mmmm

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,23 @@
 #include "main.h"
+
+/**
+ * getNumSqr - function that gets de num of sqr
+ *
+ * @num: input number
+ * @i: Counter variable
+ * Return: int
+ */
+
+int getNumSqr(int num, int i)
+{
+	if (num == (i * i))
+		return (i);
+	else if (num > (i * i))
+		return (getNumSqr(num, i + 1));
+	else
+		return (-1);
+}
+
 /**
  * _sqrt_recursion - function that returns the natural
  * square root of a number
@@ -16,22 +35,4 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	return (getNumSqr(n, 1));
-}
-
-/**
- * getNumSqr - function that gets de num of sqroot
- *
- * @num: input number
- * @i: Counter variable
- * Return: int
- */
-
-int getNumSqr(int num, int i)
-{
-	if (num == (i * i))
-		return (i);
-	else if (num > (i * i))
-		return (getNumSqr(num, i + 1));
-	else
-		return (-1);
 }
