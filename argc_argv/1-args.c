0ac2d8114371d0affdab38e79ac4204eebcd31aa
nadaMAoui <maaouinada747@gmail.com>
2022-10-19 17:14:38 +0200

    count number of arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
-*main: prgram that prints number of arguments 
-*@arg:program counter
-*Return: 0
+*main: prgram that prints number of arguments
+*@argc:program counter
+*@argv:program vector
+*Return: integer 
 */
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
-} 
+}
