f1c0c9b1765f526f7913db07255ac43dad08bddb
nadaMAoui <maaouinada747@gmail.com>
2022-10-20 09:43:41 +0200

    print the arguments it recieve

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,17 +2,17 @@
 
 /**
 * main - a program that prints all arguments it received
-* @argc: program counter 
-* @argv: program vector 
+* @argc: program counter
+* @argv: program vector
 * Return: integer
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 
 for (i = 0; i < argc; i++)
 {
-printf ("%s\n", argv[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
