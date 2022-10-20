ff4285f278377d19aaf5661af8baf0f4b68f4d67
nadaMAoui <maaouinada747@gmail.com>
2022-10-20 10:46:20 +0200

    prints the multiple of two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 /**
 * main - a program that prints the multplication of two numbers
-*@argc: program counter 
-*@argv: program vector 
-*Return: 1 success 0 null 
+*@argc: program counter
+*@argv: program vector
+*Return: 1 success 0 null
 */
 int main(int argc, char *argv[])
-{ 
-  int multiple, num1, num2;
+{
+int multiple, num1, num2;
 if (argc != 3)
 {
-    printf("%s\n", "Error");
-    return (1);
+printf("%s\n", "Error");
+return (1);
 }
-
-else 
+else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-multiple = num1 * num2;
+multiple = (num1 * num2);
 printf("%d\n", multiple);
 }
 return (0);
