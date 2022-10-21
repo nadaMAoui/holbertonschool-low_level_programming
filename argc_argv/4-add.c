5e8f5a505ebc67ecedc6f94a4ea69efeda32eddc
nadaMAoui <maaouinada747@gmail.com>
2022-10-21 14:55:35 +0200

    add positive integers ..

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -18,13 +18,13 @@ return (0);
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
-if(argv[i][j] > '9' || argv[i][j] < '0' )
+if (argv[i][j] > '9' || argv[i][j] < '0' )
 {
 printf("%s\n", "Error");
 return (1);
 }
 }
-for(k = 1; k < argc; k++ )
+for (k = 1; k < argc; k++ )
 {
 num = atoi(argv[k]);
 result += num;
