1e727654b6c1642330b7de7e081cdfa3466b1828
nadaMAoui <maaouinada747@gmail.com>
2022-10-23 12:31:18 +0200

    craete array of caracters

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 /**
 * create_array - function that create an array of caracters
-*size: number of the array elemnts 
-*c: data type
-*Return: pointer to the array  
+*@size: number of the array elemnts
+*@c: data type
+*Return: pointer to the array
 */
 char *create_array(unsigned int size, char c)
-{  
-unsigned int i = 0;
-char *buffer; 
-
-if (size == 0)
-return(NULL);
-else 
-buffer = (char*) malloc (size * sizeof(c));
 {
-if (buffer == 0)
+unsigned int i = 0;
+char *buffer;
+
+if(size == 0)
 return(NULL);
-else 
+else
+buffer = (char*) malloc (size *sizeof(c));
+{
+if(buffer == 0)
+return(NULL);
+else
 {
 for(i = 0; i < size; i++)
 *(buffer + i) = c;
 }
 }
-return(buffer); 
+return(buffer);
 }
