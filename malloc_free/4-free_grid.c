6a5bd00cbc3a184a50303de9280628aed8e6fc69
nadaMAoui <maaouinada747@gmail.com>
2022-10-24 22:09:27 +0200

    free 2d array

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-*free_grid - a function that frees a 2d array 
-*@grid: second array 
-*@height:first array 
-*Return:free 2d array 
+*free_grid - a function that frees a 2d array
+*@grid: second array
+*@height:first array
+*Return:free 2d array
 */
 void free_grid(int **grid, int height)
 {
-int i;  
+int i;
 for (i = 0; i < height; i++)
 {
 free(grid[i]);
