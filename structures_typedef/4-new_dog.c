f076bd344d50021d6259d5b7c8888c6c2e7f1e82
nadaMAoui <maaouinada747@gmail.com>
2022-10-31 17:44:06 +0100

    a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,8 +22,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
-	free(new_dog->name);
 	free(new_dog);
+	free(new_dog->name);
 	return (NULL);
 	}
 	return (new_dog);
