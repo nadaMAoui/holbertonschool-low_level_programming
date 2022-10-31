e982efddeafbce27c8faa9180e01f465ae085f03
nadaMAoui <maaouinada747@gmail.com>
2022-10-31 11:06:12 +0100

    defne structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
+/**
+ * struct_dog: a structure tof 3 members
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *Return: 0 succcess
+*/
+struct dog {
+char* name;
+float age;
+char* owner;  
+};
+#endif
