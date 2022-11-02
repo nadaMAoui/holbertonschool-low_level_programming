fd89ffb15c61e5c35e84383483d63d0b2b4b0be6
nadaMAoui <maaouinada747@gmail.com>
2022-11-02 23:29:35 +0100

    int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,19 +5,21 @@
 *@array: array of integers
 *@size: length of array
 *@cmp: pointer to the function
-*Return: integer 
+*Return: integer
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
+if (cmp != NULL && array != NULL)
+{
 if (size <= 0)
 return (-1);
-
-else if (cmp != NULL && array != NULL && size > 0)
-{
+else if (size > 0)
 for (i = 0; i < size; i++)
+{
 if (cmp(array[i]))
-return(i);
+return (i);
 }
-return(1);
+}
+return (-1);
 }
