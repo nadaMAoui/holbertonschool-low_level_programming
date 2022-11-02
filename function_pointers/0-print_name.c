995329454cd17936fb0b72b2cf6f6c5a3535b263
nadaMAoui <maaouinada747@gmail.com>
2022-11-02 12:13:20 +0100

    c1

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
-*print_name - function that prints a name 
+*print_name - function that prints a name
 *@name: user input
 *@f: pointer to the function
-*Return: void 
+*Return: void
 */
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL && name != NULL)
 f(name);
-else 
+else
 return;
 }
