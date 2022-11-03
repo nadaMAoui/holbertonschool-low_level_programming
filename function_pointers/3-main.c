eb39ffe42c763c94a71b9beeafc796b4d1fbcf2b
nadaMAoui <maaouinada747@gmail.com>
2022-11-04 00:32:38 +0100

    a program that performs simple operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+* main - a prgaram that executes five mathematical operations
+*@argc: number of arguments
+*argv: arguments of array
+*Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2, res;
+char input;
+int (*function)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit (98);
+}
+
+num1 = atoi (argv[1]);
+num2 = atoi (argv[3]);
+
+function = get_op_func(argv[2]);
+
+if (!function)
+{
+printf("Error\n");
+exit (99);
+}
+
+input = *argv[2];
+if ((input == '/'|| input == '%') && num2 == 0)
+{
+printf("Error\n");
+exit (100);
+}
+res = function(num1, num2);
+printf("%d\n", res);
+return(0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*op_add - a function that adds two integers
+*@a:first integer
+*@b:second integer
+*Return: integer
+*/
+int op_add(int a, int b)
+{
+int sum;
+sum = a + b;
+return(sum);
+}
+
+/**
+*op_sub - a function that substructs two integers
+*@a: first integer
+*@b: second integer
+*Return: integer
+*/
+int op_sub(int a, int b)
+{
+int sub;
+sub = a - b;
+return(sub);
+}
+
+/**
+*op_muö - a function that multplies two integers
+*@a: first integer
+*@b: second integer
+*Return: integer
+*/
+int op_mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
+
+/**
+*op_div - a function that divides two integers
+*@a: first integer
+*@b: second integer
+*Return: integer
+*/
+int op_div(int a, int b)
+{
+int div;
+div = a / b;
+return (div);
+}
+
+/**
+*op_mod - a function that rturnes the rest of a division of two integers
+*@a: first integer
+*@b: second integer
+*Return: integer
+*/
+int op_mod(int a, int b)
+{
+int mod;
+mod = a % b;
+return (mod);
+}
