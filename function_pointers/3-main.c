b45f6fea7d4829d7f2a94443525f25fc96510f31
nadaMAoui <maaouinada747@gmail.com>
2022-11-04 14:05:49 +0100

    get_op_funct

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,33 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, res;
-	char input;
-	int (*function)(int, int);
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+int num1, num2, res;
+char input;
+int (*function)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	num1 = atoi (argv[1]);
-	num2 = atoi (argv[3]);
+num1 = atoi (argv[1]);
+num2 = atoi (argv[3]);
 
-	function = get_op_func(argv[2]);
+function = get_op_func(argv[2]);
 
-	if (!function)
-	{
-		printf("Error\n");
-		exit (99);
-	}
-
-	input = *argv[2];
-	if ((input == '/'  ||  input == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit (100);
-	}
-	res = function(num1, num2);
-	printf("%d\n", res);
-	return (0);
+if (!function)
+{
+printf("Error\n");
+exit (99);	
+}
+input = *argv[2];
+if ((input == '/'  ||  input == '%') && num2 == 0)
+{
+printf("Error\n");
+exit (100);
+}
+res = function(num1, num2);
+printf("%d\n", res);
+return (0);
 }
