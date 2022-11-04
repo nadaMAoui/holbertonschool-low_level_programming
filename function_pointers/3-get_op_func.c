384eafc64a8272c1d32fbc706f33a85c394ae859
nadaMAoui <maaouinada747@gmail.com>
2022-11-04 13:49:05 +0100

    get_op_funct

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,28 +1,27 @@
 #include "3-calc.h"
-#include "stdlib.h"
+#include "string.h"
 /**
-*get_op_func - a function that selects the correct function to perform the operation asked by the user
-*@s: the operator passed by the user 
-*Return: integer
-*/
+ *get_op_func - a function that selects the correct function to perform the operation asked by the user
+ *@s: the operator passed by the user
+ *Return: integer
+ */
 int (*get_op_func(char *s))(int, int)
 {
   op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-while (i < 5)
-{
-if (*s == *(ops[i]).op)
-{
-return(*(ops[i]).f);
-}
-i++;
-}
-return(NULL);
+      {"+", op_add},
+      {"-", op_sub},
+      {"*", op_mul},
+      {"/", op_div},
+      {"%", op_mod},
+      {NULL, NULL}};
+  int i = 0;
+  while (ops[i].op)
+  {
+    if (strcmp(ops[i].op, s) == 0)
+    {
+      return (*(ops[i]).f);
+    }
+    i++;
+  }
+  return (NULL);
 }
