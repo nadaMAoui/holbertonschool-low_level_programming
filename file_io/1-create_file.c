194a2d047a60fdc9c56e64dfbd214e0c06663c80
nadaMAoui <maaouinada747@gmail.com>
2022-11-22 17:54:14 +0100

    create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 	for (i = 0; text_content[i]; i++)
-	;
+		break;
 	{
 		rw = write(file, text_content, i);
 		if (rw == -1)
