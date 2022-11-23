06db41578cfe9e14ebf7dd8cf8df7177460b5299
nadaMAoui <maaouinada747@gmail.com>
2022-11-23 10:45:26 +0100

    add bytes at the end of the file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - add bytes at the end of file
+ * @filename: filename
+ * @text_content: bytes of file
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, rw;
+	unsigned int i;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_RDONLY | O_APPEND);
+	if (!file)
+		return (-1);
+	if (text_content)
+		for (i = 0; text_content[i];)
+		i++;
+	rw = (file, text_content, i);
+	if (rw == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
