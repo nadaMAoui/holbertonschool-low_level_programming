94e291229499cdc9717b69d0a51090ce3236d6fb
nadaMAoui <maaouinada747@gmail.com>
2022-11-23 10:50:57 +0100

    add bytes at the end of the file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,11 +16,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!file)
 		return (-1);
 	if (text_content)
+	{
 		for (i = 0; text_content[i];)
 		i++;
-	rw = (file, text_content, i);
+	rw = write(file, text_content, i);
 	if (rw == -1)
 		return (-1);
+	}
 	close(file);
 	return (1);
 }
