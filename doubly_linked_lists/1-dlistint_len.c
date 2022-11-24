99e0fef735071ec4afbbee3a4c88daa487342059
nadaMAoui <maaouinada747@gmail.com>
2022-11-24 20:21:36 +0100

    return number of data

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,18 +2,18 @@
 /**
  * dlistint_len - return number of data
  * @h: linked list
- * return: n_data
-*/
+ * Return: n_data
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-unsigned int n_data;
-if(h == NULL)
-return(0);
-while(h)
-{
-    n_data = 1;
-    h = h->next;
-    n_data++;
-}
-return(n_data);
+	unsigned int n_data = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		n_data++;
+	}
+	return (n_data);
 }
