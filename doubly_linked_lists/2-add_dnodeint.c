22edf0f4360b72cbbdbcf70cc6540b8bc0a853be
nadaMAoui <maaouinada747@gmail.com>
2022-11-24 20:51:06 +0100

    add a node in front

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,7 +5,7 @@
  *@head: dble linked list
  *@n: int
  *Return: adress of new_node
-*/
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
@@ -13,7 +13,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-		new_node->n = n;
+
+	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
 	if (*head != NULL)
