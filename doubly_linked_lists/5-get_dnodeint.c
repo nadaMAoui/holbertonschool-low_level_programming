57bb54b9048a7c6fc05b87d8418b7008cf9496d0
nadaMAoui <maaouinada747@gmail.com>
2022-11-26 00:24:32 +0100

    find the nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index - find the Nth Node in a Linked List
+ *@head: linked list
+ *@index: index of node
+ *Return: 0 success
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current_node;
+	unsigned int length = 0;
+
+	if (!head)
+		return (NULL);
+	current_node = head;
+	while (head)
+	{
+		if (length == index)
+			return (current_node);
+		length++;
+		current_node = current_node->next;
+	}
+	return (0);
+}
