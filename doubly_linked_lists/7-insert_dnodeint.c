a61a017a2aa949b41a593faa6caaf9163aa6ceff
nadaMAoui <maaouinada747@gmail.com>
2022-11-26 03:39:15 +0100

    insert a node at idx

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,40 +4,43 @@
  * insert_dnodeint_at_index - insert new_node at idx
  *@h: linked list
  *@n: data
+ *@idx: index
  *Return: adress of new_node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *tmp;
+	dlistint_t *tmp = *h;
 	unsigned int i = 1;
 
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	new_node->n = n;
 	if (new_node == NULL)
 		return (NULL);
-	if (!(*h))
-		return (NULL);
-	else
+	if (tmp == NULL)
 	{
-		if (idx ==  0)
+		return (NULL);
+	}
+		if (idx == 0)
 			return (add_dnodeint(h, n));
 
-		else if(idx == i)
+		else if (idx == i)
 		{
-			tmp = *h;
 			for (i = 0; i < idx - 1; i++)
 				tmp = tmp->next;
 			new_node->next = tmp->next;
 			tmp->next = new_node;
-		}		
-		if(idx == i)
-		{
-			return(add_dnodeint_end(h, n));
 		}
-		if(idx > i)
-		free(new_node);
-		return(NULL);
-	}
-	return (new_node);
+		if (idx == i)
+		{
+			return (add_dnodeint_end(h, n));
+		}
+		if (idx > i)
+		{
+			free(new_node);
+		return (NULL);
+		}
+
+		return(new_node);
 }
+
