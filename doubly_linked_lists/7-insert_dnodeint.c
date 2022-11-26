fce99855d6b0672b76ed908f98adccaafe248764
nadaMAoui <maaouinada747@gmail.com>
2022-11-26 03:48:41 +0100

    insert a node at idx

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,12 +21,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-		if (idx == 0)
-			return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-		else if (idx == i)
-		{
-			while(tmp->next)
+	while(tmp->next)
+	{
+		if(idx == i)
 		{
 			new_node->prev = tmp;
 			new_node->next = tmp->next;
@@ -35,17 +35,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		tmp = tmp->next;
 		i++;
-		}
-		if (idx == i)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		if (idx > i)
-		{
-			free(new_node);
+	}
+	if (idx == i)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	if (idx > i)
+	{
+		free(new_node);
 		return (NULL);
-		}
+	}
 
-		return (new_node);
+	return (new_node);
 }
-
