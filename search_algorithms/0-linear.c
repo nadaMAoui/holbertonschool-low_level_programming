ec3aa9d23809f61a976bc1b4015f304024771906
nadaMAoui <maaouinada747@gmail.com>
2023-01-04 11:59:08 +0100

    search data algo

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
 #include <stdio.h>
+/**
+ * linear_search - function that searches for a value in a sorted array of integers
+ * @array: pointer to the vector of data
+ * @size: vector length
+ * @value: searched_data
+*/
 int linear_search(int *array, size_t size, int value)
 {
 long unsigned int i;
@@ -9,7 +15,7 @@ return(-1);
 
 for (i = 0; i < size; i++)
 {
-printf("value checked array [%ld] = [%d]\n", i, array[i]);
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 if (array[i] == value)
 break;
 }
