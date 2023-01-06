b67bbe87ccb257466516437b3b340b5072c4e0d6
nadaMAoui <maaouinada747@gmail.com>
2023-01-06 07:24:09 +0100

    BST

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -27,7 +27,7 @@ int binary_search(int *array, size_t size, int value)
 			if (i == l)
 				printf("%d", array[i]);
 			else
-				printf("%d, ", array[i]);
+				printf(", %d", array[i]);
 		}
 		printf("\n");
 
